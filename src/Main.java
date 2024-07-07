8dbbc0f671a991830157390e7875178f0f7b7173
esela22 <20210508@tiba.edu.eg>
2024-07-07 16:53:48 +0300

    ESELA

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+
 public class Main {
+
     public static void main(String[] args) {
+        int[] arr = {
+                9,
+                5,
+                2,
+                8,
+                0,
+                3,
+                1,
+                6
+        };
+        Sortable quickSort = new QuickSort();
+        quickSort.sort(arr);
+        System.out.print("Quick Sort (Descending Order): ");
+        printArray(arr);
+        int[] arr2 = {
+                9,
+                5,
+                2,
+                8,
+                0,
+                3,
+                1,
+                6
+        }; 
+        
         MergeSort mergeSort = new MergeSort();
         int[] array = {5, 8, 6, 9, 3, 1, 7};
         mergeSort.sort(array);
@@ -7,5 +34,13 @@ public class Main {
             System.out.print(numbers + " ");
         }
 
+    }
+
+    private static void printArray(int[] arr) {
+
+        for (int num: arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
