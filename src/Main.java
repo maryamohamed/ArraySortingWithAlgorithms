6075c922e8b587181e10fe35372ef407da0d51ba
Maryam mohamed <mm3279@fayoum.edu.eg>
2024-07-07 17:19:04 +0300

    project completed

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@
 public class Main {
 
     public static void main(String[] args) {
+        MergeSort mergeSort = new MergeSort();
+        int[] array = {5, 8, 6, 9, 3, 1, 7};
+        mergeSort.sort(array);
+        System.out.print("Sorted List with MergeSort : ");
+        for (int numbers : array) {
+            System.out.print(numbers + " ");
+        }
+
         int[] arr = {
                 9,
                 5,
@@ -26,15 +34,6 @@ public class Main {
                 1,
                 6
         };
-
-        MergeSort mergeSort = new MergeSort();
-        int[] array = {5, 8, 6, 9, 3, 1, 7};
-        mergeSort.sort(array);
-        System.out.print("Sorted List with MergeSort : ");
-        for (int numbers : array) {
-            System.out.print(numbers + " ");
-        }
-
     }
 
     private static void printArray(int[] arr) {
